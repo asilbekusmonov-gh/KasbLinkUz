f9af631c617bbf4d4ab20370982e2db21c97d356
asilbekusmonov-gh <asilbekusmonovv17@gmail.com>
2026-05-31 14:19:48 +0500

    feat: redirect root url to swagger

--- root/urls.py
+++ root/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from root import settings
 from root.settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # ← fix this
-    path('api/v1/', include('apps.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', RedirectView.as_view(url='/api/schema/swagger-ui/')),
+                  path('admin/', admin.site.urls),
+                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+                  path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+                  # ← fix this
+                  path('api/v1/', include('apps.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
