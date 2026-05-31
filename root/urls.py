77576e9dd48b82564d5935e76b6664f335881964
asilbekusmonov-gh <asilbekusmonovv17@gmail.com>
2026-05-31 14:08:06 +0500

    fix: swagger url

--- root/urls.py
+++ root/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+from root import settings
 from root.settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # ← fix this
     path('api/v1/', include('apps.urls')),
-] + static(MEDIA_URL, document_root=MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
