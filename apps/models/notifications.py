09e2f667d5d230f78b8e0ace8759c23c85194be7
asilbekusmonov-gh <asilbekusmonovv17@gmail.com>
2026-06-12 19:00:25 +0500

    fixed: some changes

--- apps/models/notifications.py
+++ apps/models/notifications.py
@@ -1,28 +1,21 @@
 from django.db import models
 from django.conf import settings
-from django.db.models import ForeignKey
-from django.db.models.enums import TextChoices
-from redis.commands.search.field import TextField
-from rest_framework.fields import CharField, BooleanField
-
 from apps.models.base import TimeStampedModel
 
-
 class Notification(TimeStampedModel):
-    class Type(TextChoices):
+    class Type(models.TextChoices): # Use models.TextChoices
         ORDER_STATUS = "order_status", "Order Status Update"
         NEW_MESSAGE = "new_message", "New Chat Message"
         NEW_ORDER = "new_order", "New Order Request"
 
-    recipient = ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
-    sender = ForeignKey(
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
+    sender = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name="sent_notifications"
     )
-    notification_type = CharField(max_length=20, choices=Type.choices)
-    title = CharField(max_length=150)
-    description = TextField()
-    is_read = BooleanField(default=False)
+    notification_type = models.CharField(max_length=20, choices=Type.choices)
+    title = models.CharField(max_length=150)
+    description = models.TextField() # Corrected to models.TextField
+    is_read = models.BooleanField(default=False) # Corrected to models.BooleanField
 
     class Meta:
-        app_label = "apps"
         ordering = ["-created_at"]
