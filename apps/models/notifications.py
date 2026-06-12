143f34a464575e16afa8e41ae07f8be45923c64f
asilbekusmonov-gh <asilbekusmonovv17@gmail.com>
2026-06-12 19:27:16 +0500

    fixed: some changes

--- apps/models/notifications.py
+++ apps/models/notifications.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.fields import TextField, CharField
+
 from apps.models.base import TimeStampedModel
 
+
 class Notification(TimeStampedModel):
-    class Type(models.TextChoices): # Use models.TextChoices
+    class Type(models.TextChoices):
         ORDER_STATUS = "order_status", "Order Status Update"
         NEW_MESSAGE = "new_message", "New Chat Message"
         NEW_ORDER = "new_order", "New Order Request"
@@ -12,10 +15,10 @@ class Notification(TimeStampedModel):
     sender = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name="sent_notifications"
     )
-    notification_type = models.CharField(max_length=20, choices=Type.choices)
-    title = models.CharField(max_length=150)
-    description = models.TextField() # Corrected to models.TextField
-    is_read = models.BooleanField(default=False) # Corrected to models.BooleanField
+    notification_type = CharField(max_length=20, choices=Type.choices)
+    title = CharField(max_length=150)
+    description = TextField()  # Now using models.TextField
+    is_read = models.BooleanField(default=False)  # Now using models.BooleanField
 
     class Meta:
         ordering = ["-created_at"]
