9f7ad9f008e1b58c6d1e367a8008052b9f8f22c3
asilbekusmonov-gh <asilbekusmonovv17@gmail.com>
2026-06-12 10:08:00 +0500

    fix: use is_worker and is_client properties in permissions

--- apps/permissions.py
+++ apps/permissions.py
@@ -3,12 +3,12 @@ from rest_framework.permissions import BasePermission
 
 class IsWorker(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == "user"
+        return request.user.is_authenticated and request.user.is_worker
 
 
 class IsClient(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == "customer"
+        return request.user.is_authenticated and request.user.is_client
 
 
 class IsOwner(BasePermission):
